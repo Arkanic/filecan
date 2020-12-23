fe36f29c0478292dcbb445bc62a6258b8daf2d36
Arkanic <silverfishasaurus@gmail.com>
2020-12-23 21:05:39 +0000

    more changes

--- src/server/multerconf.js
+++ src/server/multerconf.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const multer = require("multer");
 const {customAlphabet} = require("nanoid");
-const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 6);
+const nanoid = customAlphabet("0123456789abcdef", 4);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "upload");
     },
     filename: (req, file, cb) => {
-        cb(null, nanoid() + path.extname(file.originalname));
+        cb(null, (file.originalname + "-" + nanoid()).substring(0, 32) + path.extname(file.originalname));
     }
 });
 const fileFilter = (req, file, cb) => {
