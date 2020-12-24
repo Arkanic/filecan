e13ae9985c7eb91247f243ceef4ab7b2ef1be24d
Arkanic <silverfishasaurus@gmail.com>
2020-12-24 03:07:08 +0000

    probably a final format for filenames

--- src/server/multerconf.js
+++ src/server/multerconf.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const multer = require("multer");
 const {customAlphabet} = require("nanoid");
-const nanoid = customAlphabet("0123456789abcdef", 4);
+const nanoid = customAlphabet("0123456789abcdef", 6);
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "upload");
     },
     filename: (req, file, cb) => {
-        cb(null, (file.originalname + "-" + nanoid()).substring(0, 32) + path.extname(file.originalname));
+        cb(null, nanoid() + path.extname(file.originalname));
     }
 });
 const fileFilter = (req, file, cb) => {
