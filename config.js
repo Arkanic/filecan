19d6baf3585c96c2ed7862ae7d887af51af922db
Arkanic <silverfishasaurus@gmail.com>
2021-05-01 19:00:19 +0000

    fix

--- config.js
+++ config.js
@@ -15,10 +15,11 @@ const requirePassword = true;
 
 // One way encrypted password
 // use the command "node tools/genpassword.js <your password (no spaces)>" to generate the one-way hash.
-const password = "$2b$10$RuA0yFbUqtOl2i3iXC3D/uEPDVL5BHZzBAA03vW9bNh4s7yo7g1he";
+// this one is "test".
+const password = "$2b$10$cgz0yz/mv/HYZEVMiRaNqOUNi6Blay1qGX/O.0Cu8gCR1Apcz/pGS";
 
 // Specify port, if undefined defaults to 8080.
-const port = undefined || 8080;
+const port = undefined || "8080";
 
 
 module.exports = {genFilename, requirePassword, password, port};
