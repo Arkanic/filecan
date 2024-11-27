190f08ae0ffafe59c7cf24a33ed7c30aefbdca03
Arkanic <arkanic@users.noreply.github.com>
2024-11-27 19:27:09 +1300

    refactor TS, make prettier

--- src/client/client/scripts/menu.ts
+++ src/client/client/scripts/menu.ts
@@ -1,79 +1,80 @@
-import { config, sendData } from "./networking";
+import {config, sendData, XHREvent} from "./networking";
+
+const elements = {
+    body: document.getElementsByTagName("body")[0]! as HTMLBodyElement,
+    loading: document.getElementById("loading")! as HTMLDivElement,
+    content: document.getElementById("content")! as HTMLDivElement,
+    results: document.getElementById("results")! as HTMLDivElement,
+    passwordbox: document.getElementById("passwordbox")! as HTMLDivElement,
+    password: document.getElementById("password")! as HTMLInputElement,
+    form: document.getElementById("form")! as HTMLFormElement,
+    submitbutton: document.getElementById("submitbutton")! as HTMLInputElement,
+    progressbox: document.getElementById("progressbox")! as HTMLDivElement,
+    progressbar: document.getElementById("progressbar")! as HTMLProgressElement,
+    progressinfo: document.getElementById("progressinfo")! as HTMLParagraphElement,
+}
 
 export const stopLoading = () => {
-    document.getElementById("loading")?.classList.add("hidden");
-    document.getElementById("content")?.classList.remove("hidden");
+    elements.loading.classList.add("hidden");
+    elements.content.classList.remove("hidden");
 }
 
 export const setupMenu = () => {
     if (!config.requirePassword) {
-        document.getElementById("passwordbox")?.classList.add("hidden");
+        elements.passwordbox.classList.add("hidden");
     }
 }
 
 export const startFormListener = () => {
-    let form = document.getElementById("form")! as any as HTMLFormElement;
-    let xhr;
-
-    document.getElementsByTagName("body")[0].addEventListener("keypress", e => {
-        if(e.key == "Enter") document.getElementById("submitbutton")?.click();
+    elements.body.addEventListener("keypress", e => {
+        if(e.key == "Enter") elements.submitbutton.click();
     });
 
     document.getElementById("submitbutton")!.onclick = () => {
-        let formData = new FormData(form);
-        console.log(formData.get("expirylength"));
-        let action = "/api/upload";
-        xhr = sendData(formData, action, loadStart, progress, load, readyStateChange);
+        let formData = new FormData(elements.form);
+        sendData(formData, elements.password.value, "/api/upload", loadStart, progress, load, readyStateChange);
 
         return false;
     }
 
-    let progressbox = document.getElementById("progressbox")!;
-    let progressbar = document.getElementById("progressbar")! as HTMLProgressElement;
-    let progressinfo = document.getElementById("progressinfo")!;
-
-    let loading = document.getElementById("loading")!;
-    let content = document.getElementById("content")!;
-
-    let results = document.getElementById("results")!;
-
-    let dpsc;
+    let intervalID;
     let last = 0;
     let now = 0;
-    let dif;
+    let dif:number;
 
-    function loadStart(evt) {
-        progressbox.classList.remove("hidden");
-        dpsc = setInterval(() => {
+    function loadStart(e:Event):void {
+        elements.progressbox.classList.remove("hidden");
+        intervalID = setInterval(() => {
             dif = now - last;
             last = now;
         }, 1000);
     }
 
-    function progress(evt) {
-        now = evt.loaded;
-        let progress = evt.loaded / evt.total * 100;
-        progressbar.value = progress;
+    function progress(e:XHREvent):void {
+        now = e.loaded;
+        let progress = e.loaded / e.total * 100;
+        elements.progressbar.value = progress;
 
-        progressinfo.innerHTML = `
+        elements.progressinfo.innerHTML = `
         %${progress.toFixed(2)}
-        ${(evt.loaded / 1024 / 1024).toFixed(2)}mb/${(evt.total / 1024 / 1024).toFixed(2)}mb
+        ${(e.loaded / 1024 / 1024).toFixed(2)}mb/${(e.total / 1024 / 1024).toFixed(2)}mb
         ${(dif / 1024 / 1024).toFixed(2)}mb/s
-        ETA ${((evt.total - evt.loaded) / dif).toFixed(2)}s
+        ETA ${((e.total - e.loaded) / dif).toFixed(2)}s
         `;
     }
 
-    function load(evt) {
-        progressbox.classList.add("hidden");
-        content.classList.add("hidden");
-        loading.classList.remove("hidden");
+    function load(e:XHREvent):void {
+        clearInterval(intervalID);
+        elements.progressbox.classList.add("hidden");
+        elements.content.classList.add("hidden");
+        elements.loading.classList.remove("hidden");
     }
 
-    function readyStateChange(evt, xhr) {
+    function readyStateChange(e:XHREvent, xhr:XMLHttpRequest):void {
         if(xhr.readyState != 4) return;
         let json = JSON.parse(xhr.responseText);
-        loading.classList.add("hidden");
-        results.classList.remove("hidden");
+        elements.loading.classList.add("hidden");
+        elements.results.classList.remove("hidden");
         if(json.success) {
             let resultsdiv = document.createElement("div");
             resultsdiv.classList.add("border");
@@ -84,21 +85,23 @@ export const startFormListener = () => {
 
                 let filename = document.createElement("p");
                 filename.appendChild(document.createTextNode(`${json.files[i].originalname}: `));
+
                 let link = `${window.location.protocol}//${window.location.host}/${json.files[i].filename}`;
                 let linkElement = document.createElement("a");
                 linkElement.target = "_blank";
                 linkElement.href = link;
                 linkElement.appendChild(document.createTextNode(link));
+
                 filename.appendChild(linkElement);
                 itemdiv.appendChild(filename);
                 resultsdiv.appendChild(itemdiv);
             }
 
-            results.appendChild(resultsdiv);
+            elements.results.appendChild(resultsdiv);
         } else {
             let error = document.createElement("p");
             error.appendChild(document.createTextNode(json.message));
-            results.appendChild(error);
+            elements.results.appendChild(error);
         }
     }
 }
--- src/client/client/scripts/networking.ts
+++ src/client/client/scripts/networking.ts
@@ -20,17 +20,19 @@ export const getConfig = new Promise<void>(resolve => {
     xhr.send();
 });
 
-export const sendData = (formData, action, loadStart, progress, load, readyStateChange) => {
+export type XHREvent = ProgressEvent<XMLHttpRequestEventTarget>;
+export type EventCallback = (e:XHREvent) => any;
+export function sendData(formData:FormData, password:string, action:string, loadStart:EventCallback, progress:EventCallback, load:EventCallback, readyStateChange:(e:XHREvent, xhr:XMLHttpRequest) => any) {
     let xhr = new XMLHttpRequest();
     
     xhr.upload.addEventListener("loadstart", loadStart);
     xhr.upload.addEventListener("progress", progress);
     xhr.upload.addEventListener("load", load);
-    xhr.addEventListener("readystatechange", (evt) => {
-        readyStateChange(evt, xhr);
+    xhr.addEventListener("readystatechange", (e) => {
+        readyStateChange(e as XHREvent, xhr);
     });
 
     xhr.open("POST", action, true);
-    xhr.setRequestHeader("password", (document.getElementById("password")! as HTMLInputElement).value);
+    xhr.setRequestHeader("password", password);
     xhr.send(formData);
 }
