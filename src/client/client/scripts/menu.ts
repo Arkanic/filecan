a1df9997519c3d76a4884b892815e8bf9587cb5f
Arkanic <arkanic@users.noreply.github.com>
2024-11-29 22:35:52 +1300

    conjoin admin and uploader client into a horrendous frankestenian monster (for a rewrite for realz)

--- src/client/client/scripts/menu.ts
+++ src/client/client/scripts/menu.ts
@@ -1,5 +1,7 @@
 import {config, sendData, XHREvent} from "./networking";
 import WebUpload from "../../../shared/types/webupload";
+import WebLogs from "../../../shared/types/weblogs";
+import WebFiles from "../../../shared/types/webfiles";
 
 const elements = {
     body: document.getElementsByTagName("body")[0]! as HTMLBodyElement,
@@ -13,6 +15,16 @@ const elements = {
     progressbox: document.getElementById("progressbox")! as HTMLDivElement,
     progressbar: document.getElementById("progressbar")! as HTMLProgressElement,
     progressinfo: document.getElementById("progressinfo")! as HTMLParagraphElement,
+
+    adminopen: document.getElementById("adminopen")! as HTMLAnchorElement,
+    contentopen: document.getElementById("contentopen")! as HTMLAnchorElement,
+    adminlogin: document.getElementById("adminlogin")!,
+    adminpassword: document.getElementById("adminpassword")! as HTMLInputElement,
+    adminsubmit: document.getElementById("adminsubmit")! as HTMLInputElement,
+
+    admincontent: document.getElementById("admincontent")!,
+    adminfilesbox: document.getElementById("adminfilesbox")!,
+    adminlogbox: document.getElementById("adminlogbox")!
 }
 
 export const stopLoading = () => {
@@ -28,7 +40,7 @@ export const setupMenu = () => {
 
 export const startFormListener = () => {
     elements.body.addEventListener("keypress", e => {
-        if(e.key == "Enter") elements.submitbutton.click();
+        if(e.key == "Enter" && !elements.content.classList.contains("hidden")) elements.submitbutton.click();
     });
 
     document.getElementById("submitbutton")!.onclick = () => {
@@ -109,3 +121,101 @@ export const startFormListener = () => {
         }
     }
 }
+
+// admin mess
+
+function getUploadedFiles() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/admin/files", true);
+    xhr.setRequestHeader("password", elements.adminpassword.value);
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState != 4) return;
+
+        let response = JSON.parse(xhr.responseText) as WebFiles;
+        if(!response.success) return alert(`web error: ${response.message}`);
+
+        let files = response.files;
+        for(let i in files) {
+            let file = files[i];
+
+            let filediv = document.createElement("div");
+            filediv.classList.add("item-box");
+
+            let details = document.createElement("p");
+            details.appendChild(document.createTextNode(`${file.file.originalname}: ${file.views} views, c: ${new Date(file.created).toString()} / e: ${new Date(file.expires).toString()}, ${(file.filesize / 1000 / 1000).toFixed(2)}mb`));
+
+            filediv.appendChild(details);
+            elements.adminfilesbox.appendChild(filediv);
+        }
+
+        if(files.length == 0) {
+            let p = document.createElement("p");
+            let i = document.createElement("i");
+            i.appendChild(document.createTextNode("No files uploaded..."));
+            p.appendChild(i);
+
+            elements.adminfilesbox.appendChild(p);
+        }
+    }
+
+    xhr.send();
+}
+
+function updateLogs(lastUpdate:number):number {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/admin/logs", true);
+    xhr.setRequestHeader("password", elements.adminpassword.value);
+    xhr.setRequestHeader("minimumtime", lastUpdate.toString());
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState != 4) return;
+
+        let response = JSON.parse(xhr.responseText) as WebLogs;
+        if(!response.success) return alert(`web error: ${response.message}`);
+
+        let logs = response.logs;
+        for(let i in logs) {
+            let log = logs[i];
+            let elem = document.createElement("p");
+    
+            let time = new Date(log.time);
+            elem.innerHTML = `${time.toLocaleDateString()} ${time.toLocaleTimeString()} [${log.author}]: <span style="color:${log.color}">${log.content}</span>`;
+    
+            elements.adminlogbox.appendChild(elem);
+        }
+        elements.adminlogbox.scrollTop = elements.adminlogbox.scrollHeight;
+    }
+
+    xhr.send();
+
+    return Date.now();
+}
+
+let lastUpdate = 0;
+
+elements.adminsubmit.addEventListener("click", () => {
+    elements.adminlogin.classList.add("hidden");
+    elements.admincontent.classList.remove("hidden");
+
+    setInterval(() => {
+        lastUpdate = updateLogs(lastUpdate);
+    }, 1000 * 10); // 10 seconds
+    lastUpdate = updateLogs(0); // init
+
+    getUploadedFiles();
+});
+
+elements.adminpassword.addEventListener("keydown", e => {
+    if(e.key == "Enter" && !elements.adminlogin.classList.contains("hidden")) elements.adminsubmit.click();
+});
+
+elements.adminopen.addEventListener("click", () => {
+    if(!elements.adminlogin.classList.contains("hidden") || !elements.admincontent.classList.contains("hidden")) return;
+    elements.content.classList.add("hidden");
+    elements.results.classList.add("hidden");
+    elements.adminlogin.classList.remove("hidden");
+});
+
+elements.contentopen.addEventListener("click", () => {
+    elements.adminlogin.classList.add("hidden");
+    elements.content.classList.remove("hidden");
+});
