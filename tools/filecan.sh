d1a85cf098d2b10f48321c83f96bc50a10ac1847
Arkanic <50847107+Arkanic@users.noreply.github.com>
2024-11-30 12:10:11 +1300

    don't double dip installing packages

--- tools/filecan.sh
+++ tools/filecan.sh
@@ -3,7 +3,6 @@
 wget https://raw.githubusercontent.com/Arkanic/filecan/refs/heads/main/ci/include/update.sh
 chmod +x ./update.sh
 ./update.sh
-./install.sh
 
 echo ""
 echo "Done! Filecan installed."
