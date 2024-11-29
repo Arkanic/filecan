47d75cf65f8084d75a551167d1307f3cc868b3cd
Arkanic <50847107+Arkanic@users.noreply.github.com>
2024-11-30 12:37:24 +1300

    hacky fix so that config.yml isn't overwritten

--- ci/include/update.sh
+++ ci/include/update.sh
@@ -3,7 +3,10 @@
 echo "Updating filecan..."
 wget https://github.com/Arkanic/filecan/releases/latest/download/filecan.zip
 echo "Extracting..."
+# make sure config isn't overwritten
+mv config.yml cfgtemp2501.yml
 unzip -o filecan.zip
 rm filecan.zip
+mv cfgtemp2501.yml config.yml
 ./install.sh
 echo "Done!"
