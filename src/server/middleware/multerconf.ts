50c8a7040b4466be268949422eb6626ad3d09c87
Arkanic <arkanic@users.noreply.github.com>
2025-03-01 21:28:18 +1300

    adjust package.lock version

--- src/server/middleware/multerconf.ts
+++ src/server/middleware/multerconf.ts
@@ -5,8 +5,7 @@ import randomString from "../util/genrandom";
 export const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         cb(null, randomString(6) + path.extname(file.originalname));
-    },
-    
+    }
 });
 export const fileFilter = (req:any, file:any, cb:any) => {
     if(req.path == "/api/upload") cb(null, true);
